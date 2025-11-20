0213952c3b6885a82d1e2ecea3223c88f3c48d8d
Pedemontemarina <pedemari.rr@gmail.com>
2025-11-20 17:05:59 +0000

    ui node implemented and tested

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -1,15 +1,15 @@
 /*
-✓Implement a simple textual interface to retrieve the user command 
-(i.e., you can use cin (c++) or input (python). The user should be able to 
-select the robot they want to control (turtle1 or turtle2), and the velocity 
-of the robot. 
+✓Implement a simple textual interface to retrieve the user command
+(i.e., you can use cin (c++) or input (python). The user should be able to
+select the robot they want to control (turtle1 or turtle2), and the velocity
+of the robot.
 
-✓The command should be sent for 1 second, and then the robot should 
-stop, and the user should be able again to insert the command. 
+✓The command should be sent for 1 second, and then the robot should
+stop, and the user should be able again to insert the command.
 
 1) chiedere all'utente quale tartaruga controllare
 
-2) utente inserisce velocità 
+2) utente inserisce velocità
 
 3) invio il comando per 1s e poi la tarta si ferma
 
@@ -27,9 +27,10 @@ loop infinito:
     ferma la tartaruga (Twist a zero)
 */
 
+// AGGIORNA DIPENDENZEEEEE
+
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "turtlesim/msg/pose.hpp"
 #include <string>
 #include <iostream>
 #include <memory>
@@ -37,58 +38,11 @@ loop infinito:
 class TurtleController : public rclcpp::Node
 {
 public:
-    TurtleController(): Node("user_input_turtle_controller")
+    TurtleController() : Node("user_input_turtle_controller")
     {
         // Publisher per turtle1 e turtle2
         pub_turtle1_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
         pub_turtle2_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10);
-
-        // Subscriber per turtle1 e turtle2
-        sub_turtle1_ = this->create_subscription<turtlesim::msg::Pose>(
-            "turtle1/pose", 10,
-            std::bind(&TurtleController::pose_callback_turtle1, this, std::placeholders::_1));
-
-        sub_turtle2_ = this->create_subscription<turtlesim::msg::Pose>(
-            "turtle2/pose", 10,
-            std::bind(&TurtleController::pose_callback_turtle2, this, std::placeholders::_1));
-
-        // Timer per leggere input utente in loop
-        timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(1000),
-            std::bind(&TurtleController::user_input_loop, this));
-    }
-
-private:
-    // Publisher
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_turtle1_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_turtle2_;
-
-    // Subscriber
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub_turtle1_;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub_turtle2_;
-
-    // Timer
-    rclcpp::TimerBase::SharedPtr timer_;
-
-    // Stato delle tartarughe (vuoto, aggiornalo dopo input)
-    double x1_, y1_, theta1_;
-    double x2_, y2_, theta2_;
-
-    // ======================
-    // Funzioni Subscriber
-    // ======================
-    void pose_callback_turtle1(const turtlesim::msg::Pose::SharedPtr msg)
-    {
-        x1_ = msg->x;
-        y1_ = msg->y;
-        theta1_ = msg->theta;
-    }
-
-    void pose_callback_turtle2(const turtlesim::msg::Pose::SharedPtr msg)
-    {
-        x1_ = msg->x;
-        y2_ = msg->y;
-        theta_2 = msg->theta;
     }
 
     // ======================
@@ -96,61 +50,84 @@ private:
     // ======================
     void user_input_loop()
     {
-        static bool waiting_input = true;
-        if(!waiting_input) return; 
-        // Serve per evitare che il timer richiami la funzione più volte mentre stiamo 
-        // ancora leggendo l’input dall’utente. Se waiting_input è false, significa che stiamo già leggendo.
-        // In questo caso la funzione esce subito, evitando conflitti con altre chiamate del timer e setta a false.
-        // cosi la funzione non viene più chiamata
+        while (rclcpp::ok())
+        {
+            std::string turtle_name;
+            float linear_vel, angular_vel;
 
-        waiting_input = false;
+            // --- Input utente ---
+            std::cout << "Select which turtle to control (turtle1/turtle2): ";
+            std::cin >> turtle_name;
+            if (turtle_name != "turtle1" && turtle_name != "turtle2")
+            {
+                std::cout << "Name not valid!" << std::endl;
+                continue;
+            }
+            std::cout << "Linear velocity: ";
+            std::cin >> linear_vel; // geomtry_msg::Twist float64
+            if (std::cin.fail())
+            {
+                std::cin.clear();                                                   // error flag clearing
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard input
+                std::cout << "Input not valid!" << std::endl;
+                continue;
+            }
 
-        std::string turtle_name;
-        double linear_vel, angular_vel;
-        std::string direction;
+            std::cout << "Angular velocity: ";
+            std::cin >> angular_vel;
+            if (std::cin.fail())
+            {
+                std::cin.clear();                                                   // error flag clearing
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard input
+                std::cout << "Input not valid!" << std::endl;
+                continue;
+            }
 
-        // --- Input utente ---
-        std::cout << "Seleziona la tartaruga da controllare (turtle1/turtle2): ";
-        std::cin >> turtle_name;
+            // Create message
+            auto msg = geometry_msgs::msg::Twist();
+            msg.linear.x = linear_vel;
+            msg.angular.z = angular_vel;
 
-        std::cout << "Direzione da muovere (es: move_right, move_left, etc.): ";
-        std::cin >> direction;
+            // Publish message
+            if (turtle_name == "turtle1")
+            {
+                pub_turtle1_->publish(msg);
+            }
+            else if (turtle_name == "turtle2")
+            {
+                pub_turtle2_->publish(msg);
+            }
 
-        std::cout << "Velocità lineare: ";
-        std::cin >> linear_vel;
+            // wait 1 second
+            rclcpp::sleep_for(std::chrono::seconds(1));
 
-        std::cout << "Velocità angolare: ";
-        std::cin >> angular_vel;
+            // send stop command by publishing zero velocities
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.0;
 
-        
-        // --- Pubblica velocità ---
-        auto msg = geometry_msgs::msg::Twist();
-        msg.linear.x = linear_vel;
-        msg.angular.z = angular_vel;
-
-        if(turtle_name == "turtle1") {
-            pub_turtle1_->publish(msg);
-        
-        } else if(turtle_name == "turtle2") {
-            pub_turtle2_->publish(msg);
-
-        } else {
-            std::cout << "Nome tartaruga non valido!" << std::endl;
+            if (turtle_name == "turtle1")
+            {
+                pub_turtle1_->publish(msg);
+            }
+            else if (turtle_name == "turtle2")
+            {
+                pub_turtle2_->publish(msg);
+            }
+            std::cout << "Command executed for 1 second. Turtle stopped.\n";
         }
-
-        // Dopo l’invio del comando si può fermare o attendere 1 secondo
-        // TODO: logica per stop o durata comando
-
-        waiting_input = true; // riattiva il timer per nuovo input
     }
+
+private:
+    // Publisher
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_turtle1_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_turtle2_;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<TurtleController>();
-    rclcpp::spin(node);
+    node->user_input_loop();
     rclcpp::shutdown();
     return 0;
 }
-
