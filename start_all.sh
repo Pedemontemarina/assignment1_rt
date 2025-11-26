972e0f5a8bce581e33915ea22ad5d015e9c1c1ae
Pedemontemarina <pedemari.rr@gmail.com>
2025-11-26 15:15:35 +0000

    minor updates

--- start_all.sh
+++ start_all.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Name of the tmux session
-SESSION="ros_nodes"
+SESSION="assignment1_rt_session"
+
+# If the session already exists, kill it
+tmux has-session -t $SESSION 2>/dev/null
+if [ $? == 0 ]; then
+    tmux kill-session -t $SESSION
+fi
+
+# Function to handle Ctrl+C
+cleanup() {
+    echo ""
+    echo "Ctrl+C detected. Killing tmux session..."
+    tmux kill-session -t $SESSION
+    exit 0
+}
+
+# Set trap for SIGINT (Ctrl+C)
+trap cleanup SIGINT
+
+# Create new tmux session
 tmux new-session -d -s $SESSION
 
 # =========================
@@ -11,7 +29,7 @@ tmux rename-window "launch"
 tmux send-keys "source /home/ubuntu/ros_ws2/install/setup.bash && ros2 launch assignment1_rt assignment_launch.py" C-m
 
 # =========================
-# Terminal 2: turtle_spawn
+# Terminal 2: turtle_spawn node
 # =========================
 tmux new-window -t $SESSION -n "turtle_spawn"
 tmux send-keys "source /home/ubuntu/ros_ws2/install/setup.bash && ros2 run assignment1_rt turtle_spawn" C-m
@@ -28,4 +46,5 @@ tmux send-keys "source /home/ubuntu/ros_ws2/install/setup.bash && ros2 run assig
 tmux new-window -t $SESSION -n "ui_node"
 tmux send-keys "source /home/ubuntu/ros_ws2/install/setup.bash && ros2 run assignment1_rt ui_node" C-m
 
+# Attach to the tmux session
 tmux attach-session -t $SESSION
